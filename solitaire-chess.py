d9c1409bb2818297b880d6639a53ab47a977da8c
Jean <jeanhernandezmeze@gmail.com>
2016-11-26 00:43:02 -0300

    Added function that draws menu given a title and a list of options. Added function that given a string formats it's png photo to 200x50. Added delete and max length features to Leer(). Added Main Menu Added sprites

--- solitaire-chess.py
+++ solitaire-chess.py
@@ -1,38 +1,102 @@
 import pygame, sys, re
 from pygame.locals import *
-ventana = pygame.display.set_mode(600,600)
-pygame.display.set_caption("Solitaire Chess")
-
 
 #funcion que cierra pygame y cierra el programa
 def cerrar():
     #precondicion true
-
-    #postcondicion true
     pygame.quit()
     sys.exit()
+    # postcondicion true
 
 #funcion que detecta eventos y evalua si son teclas
-def Leer():
+#y retorna la entrada del usuario
+def Leer(x,y, color, longitud_maxima):
     #precondicion true
     string = ""
-    patron = re.compile("^[a-zA-Z0-9_ ]*$")
+    patron = re.compile("^\w{1}$")
+    patron2 = re.compile("\w")
     while True:#(funcion de cota???, invariante???)
         for event in pygame.event.get():
             if event.type == QUIT:
                     cerrar()
             if event.type == KEYDOWN:
-                if patron.match(pygame.key.name(event.key)):
+                print(event.key)
+                if patron.match(pygame.key.name(event.key)) != None or pygame.key.name(event.key) == "-" and \
+                                len(string) < longitud_maxima:
+                    # dibujar la letra, sumarle pixeles dependiendo de la longitud para que quede mas lejos la letra nueva
+                    # recordar que se debe usar el color pasado por parametro
                     string += pygame.key.name(event.key)
-                elif
-                    event.key == K_KP_ENTER:
-                        #postcondicion string solo contiene elementos validos
-                        try:
-                            assert(patron.match(string))
-                        except:
-                            cerrar()
+                elif event.key == 13:
+                    #postcondicion string solo contiene elementos validos
+                    try:
+                        assert(patron2.match(string) != None or len(string) == 0)
+                        print(string)
                         return string
+                    except:
+                        print("Error en la lectura")
+                        cerrar()
+                elif event.key ==8 and len(string)>0:
+                    string = string[:len(string)-1]
+
+
+#funcion que recibe un string del mensaje de la opcion
+#y carga el sprite correspondiente y lo lleva a resolucion
+#150x50
+def formatearOpcion(opcion):
+    #precondicion existe la imagen
+    return pygame.transform.scale(pygame.image.load("sources/sprites/" + opcion + ".png"), (200,50))
+    #postcondicion true
+
+
+#funcion que dibuja un menu y sus opciones
+def dibujarMenu(titulo, opciones):
+    #precondicion al menos debe haber 1 opcion y el titulo debe no ser nulo
+    #y todas las opciones no deben ser nulas
+    try:
+        assert(len(opciones) > 0 and titulo != "" and all(opcion != "" for opcion in opciones))
+    except:
+        print("Error con los datos del menu")
+        cerrar()
+    titulo_menu = pygame.transform.scale(pygame.image.load("sources/sprites/" + titulo + ".png"), (400, 100))
+    ventana.blit(titulo_menu, (100, 180))
+    imagenes = []
+    for opcion in opciones:
+            imagenes.append(formatearOpcion(opcion))
+    for i in range(len(imagenes)):
+        ventana.blit(imagenes[i], (200, 300 + (i * 60)))
+    #postcondicion true
 
 #funcion que maneja el menu principal
 def MenuPrincipal():
     #precondicion true
+    #mostrar opciones al usuario
+    color_cielo = pygame.Color(25,158,218)
+    color_lectura = pygame.Color(147,55,120)
+    ventana.fill(color_cielo)
+    ventana.blit(imagenTitulo, (150,20))
+    pygame.display.update()
+    dibujarMenu("menuprincipal", ["partidanueva", "cargarpartida", "mostrarrecords", "salirjuego"])
+    pygame.display.update()
+    x = 10
+    y = 10
+    while True:
+        opcion = Leer(x,y, color_lectura,1)
+        if opcion == "1":
+            print("JUGAR!")
+        elif opcion == "2":
+            print("Cargar")
+        elif opcion == "3":
+            print("Records")
+        elif opcion == "4":
+            print("seguro??")
+            cerrar()
+        else:
+            print("Error opcion invalida")
+
+pygame.init()
+ventana = pygame.display.set_mode((600,600))
+pygame.display.set_caption("Solitaire Chess")
+imagenTablero = pygame.image.load("sources/sprites/tablero.jpg")
+imagenTitulo = pygame.transform.scale(pygame.image.load("sources/sprites/title.png"), (300,150))
+fuente = pygame.font.Font(None, 36)
+MenuPrincipal()
